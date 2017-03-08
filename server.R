39b0caf7d7dbc7de2b809a416a0d615906fb77b1
Ian Wohlers <ianw9@uw.edu>
2017-03-07 22:08:30 -0800

    Polish map and add details to interactive hover text-blurbs

--- server.R
+++ server.R
@@ -37,7 +37,9 @@ server <- function(input, output) {
     
     #input$show.schools.key
     if(input$show.schools.key){
-      m <- addCircles(m, lng=df.public.schools$Longitude, lat=df.public.schools$Latitude, weight = 0, radius = 100, popup = paste0(df.public.schools$schoolName, ": ranked above ", df.public.schools$rankStatewidePercentage, "% of Seattle schools"), fillOpacity = 0.8, color = "Grey")
+      m <- addCircles(m, lng=df.public.schools$Longitude, lat=df.public.schools$Latitude, weight = 0, radius = 50 + df.public.schools$numberOfStudents/20, 
+        popup = paste0(df.public.schools$schoolName, " (", df.public.schools$numberOfStudents, " students): ranked above *", df.public.schools$rankStatewidePercentage, "% of Seattle schools. (Demographics: ", df.public.schools$percentofWhiteStudents, "% White, ", df.public.schools$percentofAfricanAmericanStudents, "% African American, ", df.public.schools$percentofAsianStudents, "% Asian, ", df.public.schools$percentofHispanicStudents, "% Hispanic, and ", df.public.schools$percentofTwoOrMoreRaceStudents, "% Mixed Race. ", df.public.schools$percentFreeDiscLunch, "% of students at this school are on free/discounted lunch)" ), 
+        fillOpacity = 0.8, color = "Grey")
     }
     
     return(m)  # Print the map
