73fc5acfc1f6302640235c4efa5ca4850a2c94f7
Roger Klaaskate <rogsoccerman@gmail.com>
2017-03-08 19:02:34 -0800

    Redo page to use navbar

--- server.R
+++ server.R
@@ -1,4 +1,3 @@
-#install.packages("plotly")
 library("ggplot2")
 library("shiny")
 library("dplyr")
@@ -11,20 +10,19 @@ server <- function(input, output) {
   
   # For the map
   public.schools.filtered <- reactive({
-    filter(df.public.schools, (schoolLevel == "Elementary" & input$elem.key) | (schoolLevel == "Middle" & input$middle.key) | (schoolLevel == "High" & input$high.key)) %>% 
-<<<<<<< HEAD
-      filter(input$School.Rank[1] <= rankStatewidePercentage & input$School.Rank[2] >= rankStatewidePercentage) %>% 
-      filter(input$african.american.percentage.key[1] <= PercentOfAfricanAmericanStudents & input$african.american.percentage.key[2] >= PercentOfAfricanAmericanStudents) 
+    df.schools.searched <- df.public.schools[grep(tolower(input$school.search.key), tolower(df.public.schools$NAME)),] %>% #Look up schools with matching strings
+      filter((schoolLevel == "Elementary" & input$elem.key) | (schoolLevel == "Middle" & input$middle.key) | (schoolLevel == "High" & input$high.key)) %>% #Filter by school level
+      filter(input$free.reduced.lunch.percentage.key[1] <= percentFreeDiscLunch & input$free.reduced.lunch.percentage.key[2] >= percentFreeDiscLunch) #Fiter by # of African Americans
+    
+    return(df.schools.searched)
   })
   
-
-=======
-      filter(input$african.american.percentage.key[1] <= percentofAfricanAmericanStudents & input$african.american.percentage.key[2] >= percentofAfricanAmericanStudents) 
-  })
+  test <- df.public.schools %>% 
+    filter(SCHOOL, contains("g"))
+  
   
   pal <- colorQuantile("RdBu", df.public.schools$rankStatewidePercentage, n = 7)
   
->>>>>>> e177a6a5f388d11514101df98b932a81e1a7bde6
   # plots the map of seattle
   output$seattle_map <- renderLeaflet({
     public.schools.map.data <- public.schools.filtered()
@@ -44,31 +42,38 @@ server <- function(input, output) {
     
     #input$show.schools.key
     m <- addCircles(m, lng=df.public.schools$Longitude, lat=df.public.schools$Latitude, weight = 0, radius = 50 + df.public.schools$numberOfStudents/20, 
-      popup = paste0(df.public.schools$schoolName, " (", df.public.schools$numberOfStudents, " students): ranked above ", df.public.schools$rankStatewidePercentage, "% of Seattle schools. (Demographics: ", df.public.schools$percentofWhiteStudents, "% White, ", df.public.schools$percentofAfricanAmericanStudents, "% African American, ", df.public.schools$percentofAsianStudents, "% Asian, ", df.public.schools$percentofHispanicStudents, "% Hispanic, and ", df.public.schools$percentofTwoOrMoreRaceStudents, "% Mixed Race. ", df.public.schools$percentFreeDiscLunch, "% of students at this school are on free/discounted lunch)" ), 
-      fillOpacity = 0.8 * as.numeric(!is.na(match(df.public.schools$OBJECTID, public.schools.filtered()$OBJECTID)) & input$show.schools.key), #Rather than redraw points, just make unselected points (points not present in the filtered database) opaque; as.numeric(FALSE) = 0, AKA fully transluctent
-      color = "Grey")
+                    popup = paste0("<b>", df.public.schools$schoolName, "</b> (", df.public.schools$numberOfStudents, " students): </br>-Ranked above ", df.public.schools$rankStatewidePercentage, "% of Seattle schools. </br>-", df.public.schools$percentFreeDiscLunch, "% of students at this school are on free/discounted lunch)",  "</br></br>(Demographics: ", df.public.schools$percentofWhiteStudents, "% White, ", df.public.schools$percentofAfricanAmericanStudents, "% African American, ", df.public.schools$percentofAsianStudents, "% Asian, ", df.public.schools$percentofHispanicStudents, "% Hispanic, and ", df.public.schools$percentofTwoOrMoreRaceStudents, "% Mixed Race)"), 
+                    fillOpacity = 0.8 * as.numeric(!is.na(match(df.public.schools$OBJECTID, public.schools.filtered()$OBJECTID)) & input$show.schools.key), #Rather than redraw points, just make unselected points (points not present in the filtered database) opaque; as.numeric(FALSE) = 0, AKA fully transluctent
+                    color = "Grey")
     
     return(m)  # Print the map
   })
-
-    # plots graph that the x and y axis can be changed
-
-  output$seattle_plot <- renderPlot({
-    x.label <- gsub("([[:upper:]][[:lower:]])", "\\2 \\1", input$x.var.key)
-    y.label <- gsub("([[:upper:]][[:lower:]])", "\\2 \\1", input$y.var.key)
-    p <- ggplot(data = reactive.data())+
-      geom_point(mapping = aes(x = get(input$x.var.key), y = get(input$y.var.key), text = SCHOOL)) +
-      geom_smooth(mapping = aes(x = get(input$x.var.key), y = get(input$y.var.key))) +
-      labs(x = x.label, y = y.label)
+  
+  axis.names <- reactive({
+    axis.names.list <- list("rankStatewidePercentage" = "% of Schools this ranked above", 'percentofAfricanAmericanStudents' = "% African American Students", 'percentofWhiteStudents' = "% White Students", 'percentFreeDiscLunch' = '% Free/Disc Lunch', "pupilTeacherRatio" = "Pupil teacher ratio", 'percentofAsianStudents' = "% Asian American Students")
     
-    #plot_ly(df.public.schools, x = ~percentofWhiteStudents, y = ~percentFreeDiscLunch) %>% 
+    x.name <- axis.names.list[[input$x.var.key]]
+    y.name <- axis.names.list[[input$y.var.key]]
+    
+    return(c(x.name, y.name))
+  })
+  
+  # Plots graph, x and y axis can be changed
+  output$seattle_plot <- renderPlotly({
+    p <- plot_ly(filter(df.public.schools, !is.na(rankStatewidePercentage)), x = ~get(input$x.var.key), y = ~get(input$y.var.key), type = 'scatter', mode = 'markers',
+                 hoverinfo = 'text',
+                 alpha = 0.8,
+                 size = ~numberOfStudents,
+                 text = ~paste0('School: ', SCHOOL, 
+                                '</br>', axis.names()[1], ': ', get(input$x.var.key), "%" , 
+                                '</br>', axis.names()[2], ': ', get(input$y.var.key), "%" ,
+                                '</br> (School size:', numberOfStudents, ' students)')) %>% 
+      layout(xaxis = list(title = axis.names()[1]), yaxis = list(title = axis.names()[2]))
     
     
+    p <- ggplotly(p)
     return(p)
   })
-<<<<<<< HEAD
-
-=======
   
   output$seattle_table <- renderTable({
     zip.summaries <- df.public.schools %>%
@@ -76,7 +81,6 @@ server <- function(input, output) {
       summarise(round(mean(rankStatewidePercentage, na.rm = TRUE)), round(mean(percentofAfricanAmericanStudents)), mean(percentFreeDiscLunch), round(mean(percentofWhiteStudents)))
   })
   
->>>>>>> e177a6a5f388d11514101df98b932a81e1a7bde6
 }
 
 #creates the server out of the server function
